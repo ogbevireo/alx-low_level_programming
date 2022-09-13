3c7dba5187b0e964720dea429f07519499abcd2d
ogbevireo <ogbevire@gmail.com>
2022-09-13 16:18:55 -0700

    Check if output is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _isalpha - Check if output is an alphabet or not
+*
+* @c: - Integer variable to output the characters in ASCII code
+*
+* Return: Always 0 (success)
+*/
+int _isalpha(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
