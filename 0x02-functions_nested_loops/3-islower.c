32c3781ac561e1301ccb0491e9f09e9c8fb60af4
ogbevireo <ogbevire@gmail.com>
2022-09-13 16:13:06 -0700

    Check for lowercase characters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
 * _islower - Check if output is in lowercase or otherwise
-* @c - Integer variable to output the characters in ASCII code
+*
+* @c: - Integer variable to output the characters in ASCII code
+*
 * Return: Always 0 (success)
 */
 int _islower(int c)
