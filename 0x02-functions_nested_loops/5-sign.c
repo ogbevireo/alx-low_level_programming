f88a96885aaaf0d7c7d437c722c5fbf1136fee9d
ogbevireo <ogbevire@gmail.com>
2022-09-13 17:10:27 -0700

    print the sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,14 +13,14 @@ int print_sign(int n)
 		return (1);
 		_putchar(43);
 	}
-	else if (n == 0)
-	{
-		return (0);
-		_putchar(48);
-	}
-	else
+	else if (n < 0)
 	{
 		return (-1);
 		_putchar(45);
+	}
+	else
+	{
+		return (0);
+		_putchar(48);
 	}
 }
