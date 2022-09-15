073df50299de35cbeec567e6f1cc5eaeb67aff4a
ogbevireo <ogbevire@gmail.com>
2022-09-15 05:53:10 -0700

    multiply two numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,12 +5,12 @@
 * @a: - Integer variable to be multiplied
 * @b: - Integer variable to be multiplied
 *
-* Return: Always 0 (success)
+* Return: Always result
 */
 int mul(int a, int b)
 {
 	int a, b, result;
 
 	result = a * b;
-	return (0);
+	return (result);
 }
