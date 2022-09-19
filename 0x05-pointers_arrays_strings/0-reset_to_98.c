00cd04a7d9b48be3469f0eda667f690d3d93bfb2
ogbevireo <ogbevire@gmail.com>
2022-09-19 11:27:37 -0700

    created main.h and wrote a pointer function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include main.h
+
+/**
+ * main - a function takes a pointer to an int and updates the value
+ *
+ * @n: pointer to an integer
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
