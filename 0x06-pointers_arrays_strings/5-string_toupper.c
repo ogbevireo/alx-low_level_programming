ee81cef49c5e0c0de66269f03b2a64eb066846e5
ogbevireo <ogbevire@gmail.com>
2022-09-22 04:36:26 -0700

    change lowercase strings to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - this converts lowercase to uppercase
+ * @s: the input string
+ * Return: the pointer to the destination
+ */
+char *string_toupper(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
+	}
+	return (s);
+}
