e15bf0b32a4198e54245045a4376fd8a2b2f0c25
ogbevireo <ogbevire@gmail.com>
2022-09-26 13:51:31 -0700

    copy a point of memory to another

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - copies from one part of memory to another
+ * @dest: destination in memory
+ * @src: source in memory
+ * @n:n bytes to be filled
+ * Return: returns the pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i)
+
+	return (dest);
+}
