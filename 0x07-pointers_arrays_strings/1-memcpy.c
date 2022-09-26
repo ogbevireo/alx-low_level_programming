fc26ed0b14909b91e14f54510ab08634d0cd51fc
ogbevireo <ogbevire@gmail.com>
2022-09-26 14:20:37 -0700

    copy a point of memory to another

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,8 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		*(dest + i) = *(src + i)
-	_memcpy(dest, src, sizeof(src + 1));
+		*(dest + i) = *(src + i);
 
 	return (dest);
 }
