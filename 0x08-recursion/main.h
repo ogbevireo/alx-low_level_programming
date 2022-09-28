6ee8786e24057b0af85c906b8c97e9ec9fdcd233
ogbevireo <ogbevire@gmail.com>
2022-09-28 20:29:02 +0100

    print prime numbers

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int power_operation(int n, int c);
-
+int is_prime_number(int n);
+int is_prime(unsigned int n, unsigned int c);
 
 #endif
