3dcfaf046bd457d15e5cfc10353e489ba9af8043
ogbevireo <ogbevire@gmail.com>
2022-09-29 05:54:08 +0100

    compares two strings

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,6 +13,7 @@ int is_prime_number(int n);
 int is_prime(unsigned int n, unsigned int c);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int wildcmp(char *s1, char *s2);
 
 
 #endif
