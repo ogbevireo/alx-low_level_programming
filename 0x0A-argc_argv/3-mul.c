06d3a911839341a7eb5752bb5fa0186041f7ec73
ogbevireo <ogbevire@gmail.com>
2022-09-30 21:43:42 +0100

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints the number of arguments passed into it
+ * @argc: argument count
+ * @argv: argument parameter
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+}
