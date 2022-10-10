5db610ab8eb6c274a66f422777ee00098d4f47a2
ogbevireo <ogbevire@gmail.com>
2022-10-10 16:28:42 +0100

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - funtion to print the file from which the program is called
  * Return: always returns 0
  *
  */
-#include <stdio.h>
-#include <stdlib.h>
 
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
